a4b6f0f943b6ac59f4558dc51bfbb819d2a8adea
Victorm200 <victormwaniki2005@gmail.com>
2025-07-27 23:12:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Toggle dark mode
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
 
+// Toggle tip content visibility
+const titles = document.querySelectorAll(".tip-title");
+
+titles.forEach(title => {
+  title.addEventListener("click", () => {
+    const content = title.nextElementSibling;
+    content.classList.toggle("show");
+  });
+});
